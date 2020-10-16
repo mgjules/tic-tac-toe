19f2261b6936e8b189295754651d9ef3a382ba17
Jules Michael <julesmichaelgiovanni@gmail.com>
2020-10-16 23:53:42 +0400

    test: create mock repository

---
+++ game/repository/mock.go
@@ -0,0 +1,44 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/mgjules/tic-tac-toe/game"
+)
+
+// Mock represents a Mock repository
+type Mock struct {
+	db map[string]*game.Game
+}
+
+// NewMock returns a new Mock repository
+func NewMock() *Mock {
+	return &Mock{
+		db: map[string]*game.Game{},
+	}
+}
+
+// LoadGame returns a game from a game id
+func (m *Mock) LoadGame(ctx context.Context, id string) (*game.Game, error) {
+	g, found := m.db[id]
+	if !found {
+		return nil, game.ErrGameNotFound
+	}
+
+	if err := g.Validate(); err != nil {
+		return nil, err
+	}
+
+	return g, nil
+}
+
+// SaveGame saves a game
+func (m *Mock) SaveGame(ctx context.Context, g game.Game) error {
+	if err := g.Validate(); err != nil {
+		return err
+	}
+
+	m.db[g.ID] = &g
+
+	return nil
+}
--- http/server_test.go
+++ http/server_test.go
@@ -8,18 +8,23 @@ import (
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/mgjules/tic-tac-toe/game"
+	"github.com/mgjules/tic-tac-toe/game/repository"
 	rhttp "github.com/mgjules/tic-tac-toe/http"
 	"github.com/stretchr/testify/suite"
 )
 
 type ServerTestSuite struct {
 	suite.Suite
-	server *rhttp.Server
+	server         *rhttp.Server
+	mockRepository game.Repository
 }
 
 func (suite *ServerTestSuite) SetupTest() {
+	suite.mockRepository = repository.NewMock()
+
 	suite.server = rhttp.NewServer(false)
-	suite.server.Routes()
+	suite.server.Routes(suite.mockRepository)
 }
 
 // TestRoutes is an integration test
@@ -60,26 +65,54 @@ func (suite *ServerTestSuite) TestHandleHealthCheck() {
 // TestHandleMove is a unit test
 // to test the move handler
 func (suite *ServerTestSuite) TestHandleMove() {
-	w := httptest.NewRecorder()
-	c, _ := gin.CreateTestContext(w)
-
-	requestBody, err := json.Marshal(struct {
+	type Request struct {
 		GameID   string `json:"game_id"`
 		Mark     uint8  `json:"mark"`
 		Position uint8  `json:"position"`
-	}{
-		GameID:   "ababa",
-		Mark:     1,
-		Position: 5,
-	})
-	if err != nil {
-		suite.Error(err)
 	}
 
-	c.Request, _ = http.NewRequest("POST", "/hola", bytes.NewBuffer(requestBody))
-	suite.server.HandleMove()(c)
+	const gameID = "123123"
+	const X = 1
+	const O = 2
 
-	suite.Equal(http.StatusOK, w.Code)
+	cases := []struct {
+		name    string
+		request Request
+		code    int
+	}{
+		{"correct move by X", Request{gameID, X, 1}, http.StatusOK},
+		{"forbidden move by O", Request{gameID, O, 1}, http.StatusForbidden},
+		{"out-of-bound move by O", Request{gameID, O, 9}, http.StatusBadRequest},
+		{"wait-your-turn move by X", Request{gameID, X, 2}, http.StatusForbidden},
+		{"correct move by O", Request{gameID, O, 0}, http.StatusOK},
+		{"forbidden move by X", Request{gameID, X, 0}, http.StatusForbidden},
+		{"correct move by X", Request{gameID, X, 2}, http.StatusOK},
+		{"correct move by O", Request{gameID, O, 6}, http.StatusOK},
+		{"correct move by X", Request{gameID, X, 5}, http.StatusOK},
+		{"winning move by O", Request{gameID, O, 3}, http.StatusOK},
+	}
+
+	for _, tc := range cases {
+		suite.Run(tc.name, func() {
+			var err error
+			w := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(w)
+
+			requestBody, err := json.Marshal(tc.request)
+			if err != nil {
+				suite.Error(err)
+			}
+
+			c.Request, err = http.NewRequest("POST", "/move", bytes.NewBuffer(requestBody))
+			if err != nil {
+				suite.Error(err)
+			}
+
+			suite.server.HandleMove(suite.mockRepository)(c)
+
+			suite.Equal(tc.code, w.Code)
+		})
+	}
 }
 
 func TestServerTestSuite(t *testing.T) {
