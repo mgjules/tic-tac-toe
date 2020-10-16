ba23d9df5a569bb5db5c25470fabe865f5ebad6e
Jules Michael <julesmichaelgiovanni@gmail.com>
2020-10-16 18:17:39 +0400

    refactor: State change to Game for better understanding

--- game/game.go
+++ game/game.go
@@ -6,25 +6,25 @@ import (
 	ttt "github.com/shurcooL/tictactoe"
 )
 
-// State represents a game state
-type State struct {
+// Game represents a game
+type Game struct {
 	ID         string
 	LastMoveBy uint8
-	BoardCells [9]uint8
+	Board      [9]uint8
 }
 
-// Validate validates the game state
-func (s *State) Validate() error {
-	if len(s.ID) == 0 {
+// Validate validates the game
+func (g *Game) Validate() error {
+	if len(g.ID) == 0 {
 		return errors.New("invalid game")
 	}
 
-	if ttt.State(s.LastMoveBy) != ttt.O && ttt.State(s.LastMoveBy) != ttt.X {
+	if ttt.State(g.LastMoveBy) != ttt.O && ttt.State(g.LastMoveBy) != ttt.X {
 		return errors.New("invalid player")
 	}
 
 	// check each cell in board
-	for _, cell := range s.BoardCells {
+	for _, cell := range g.Board {
 		switch ttt.State(cell) {
 		case ttt.O, ttt.X, ttt.F:
 			// we are good
--- game/repository.go
+++ game/repository.go
@@ -2,9 +2,9 @@ package game
 
 // Repository represents a game repository interface
 type Repository interface {
-	// LoadState returns a game state from an id
-	LoadState(id string) (State, error)
+	// LoadGame returns a game from a game id
+	LoadGame(id string) (Game, error)
 
-	// SaveState saves a game state
-	SaveState(state State) error
+	// SaveGame saves a game
+	SaveGame(state Game) error
 }
